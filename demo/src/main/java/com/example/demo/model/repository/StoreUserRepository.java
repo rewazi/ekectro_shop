de197c0ba21106b9c5463755a88acadafee999c3
rewazi <maksim.koroljov@ivkhk.ee>
2025-04-10 17:53:57 +0300

    Delete demo directory

--- demo/src/main/java/com/example/demo/model/repository/StoreUserRepository.java
+++
@@ -1,9 +0,0 @@
-package com.example.demo.model.repository;
-
-import com.example.demo.model.entity.StoreUser;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
-
-public interface StoreUserRepository extends JpaRepository<StoreUser, Long> {
-    Optional<StoreUser> findByUsername(String username);
-}
