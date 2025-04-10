6b004f6d17c213b05c032485f42a8ef6138d2b1b
rewazi <maksim.koroljov@ivkhk.ee>
2025-04-10 21:12:22 +0300

    Delete src/main/java/org/example/electro_shop/model/repository/EquipmentRepository.java

--- src/main/java/org/example/electro_shop/model/repository/EquipmentRepository.java
+++
@@ -1,9 +0,0 @@
-package org.example.electro_shop.model.repository;
-
-import org.example.electro_shop.model.entity.Equipment;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
-
-@Repository
-public interface EquipmentRepository extends JpaRepository<Equipment, Long> {
-}
